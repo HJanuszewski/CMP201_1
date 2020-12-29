9c9c5a317250eabc58d3325dda1287eed8259be1
HJanuszewski <76430960+HJanuszewski@users.noreply.github.com>
2020-12-29 10:06:13 +0100

    Implemented (but not tested) part of the Rabin-Karp

--- StringSearch/main.cpp
+++ StringSearch/main.cpp
@@ -10,17 +10,34 @@
 unsigned short FindInLine(std::string content, std::string substring)
 {
 
+	if (content.length() < substring.length())
+	{
+		return 0;
+	}
+
 	// hash = (ascii value)* (94)^(.length() - 1 ) + (next ascii value) * (94)^(.length() -2 ) + ...
 	// ascii value of the character we're supposed to hash
 	// 94 because there's that many printable ascii characters that can appear
 	unsigned int hash_substring = 0;
-
+	unsigned int hash_checked = 0;
+	unsigned int move = 0;
 	for (int i = substring.length() -1 ; i >= 0 ; i--)
 	{
 		hash_substring += (substring[i] * (94 ^ substring.length() - i) % 1009);
 	}
 
 
+	std::cout << hash_substring << std::endl;
+	
+	for (int i = 0+move; i < substring.length() +move; i++)//first hash i guess?
+	{
+		hash_checked += (content[i] * (94 ^ (substring.length() - i)) % 1009);
+	}
+	for (move = 0; move < content.length() - substring.length(); move++)
+	{
+		
+	}
+	return hash_substring;
 	//im too tired to work on this longer today I think. Continue by implementing a function or sth to change the value of the hash that is to be examined. Make sure that the hashing function was in fact properly implemented and does not just spew out random garbage (does it even compile lmao)
 
 
@@ -61,7 +78,7 @@ int main()
 {
 	//TODO: understand and implement the good suffix rule
 	//TODO: find in the lectures all of the methods we're supposed to measure the performance of out programs and start to think about implementing them
-	std::cout << "count of substrings found: " <<subStringCount("test.txt", "Raskolnikov");
+	std::cout << "count of substrings found: " <<subStringCount("test.txt", "piesek") ;
 	
 	return 0;
 
