f4e85493f58a2cf175442ee35c33c6ca30639cc0
HJanuszewski <76430960+HJanuszewski@users.noreply.github.com>
2021-01-03 12:49:36 +0100

    Final Rabin-Karp

--- StringSearch/main.cpp
+++ StringSearch/main.cpp
@@ -1,48 +1,49 @@
 ﻿#include <iostream>
 #include <fstream>
-#include <set>
-#include <map>
-#include <unordered_map>
 #include <string>
-#include <cmath>
 #include <sstream>
+#include <chrono>
+
+using the_clock = std::chrono::steady_clock;
 
 
-unsigned long long p = 1/94; // a setup so we won't have to either hardcode the value of p that should be dependant on the lenght of the substring, or make another loop, that'd only serve to set p. Instead we start p at a value that appears weird, but will account for later loop making one to many p=p*53%1009 operations.
 
+int pown = 1/97; // a setup so we won't have to either hardcode the value of p that should be dependant on the lenght of the substring, or make another loop, that'd only serve to set p. Instead we start p at a value that appears weird, but will account for later loop making one to many p=p*53%1009 operations.
+//since calculations are done wiht modulo, there is no risk of overflowing int when multiplying by 97 many times
 
-unsigned short rabinKarp(short sub_hash,short content_hash,std::string substring, std::string content)
+unsigned int rabinKarp(int &sub_hash,int &content_hash,std::string &substring, std::string content)
 {
+
 	
-	unsigned short count = 0;
-	for (int i = 0; i < content.length() - substring.length(); i++)
+	unsigned int count = 0;
+	for (int i = 0; i <= content.length() - substring.length(); i++)
 	{
-		//std::cout << sub_hash << " : " << content_hash << std::endl;
+		
 		if (sub_hash == content_hash) // the hashes match
 		{
 			
 			int j = 0;// this variable is not defined in the loop, as we need to check it's value to determine if the loop broke or not
-			for (; j < substring.length(); j++)
+			for (; j <substring.length(); j++)
 			{//	if the letters don't match - break out of this loop
 				//std::cout << substring[j] << " : " << content[j + i] << std::endl;
-				if (substring[j] != content[j + i])
+				if (substring[j] != content[i+j])
 				{
-					//std::cout << "I'm outta here!" << std::endl;
+					
 					break;
 				}
 			}
 		  //if the previous loop finished without breaking - increment count, else - keep going 
 			if (j == substring.length() )
 			{
-				
+				//we have found a match, increment count
 				count++;
 			}
 			else continue;
 		}
 		else
-		{
-			content_hash = ((content_hash - ((short)content[i] * p)) * 94 + content[i + substring.length()]) % 1009;
-			if (content_hash < 0) content_hash += 1009;
+		{// hash		=  ((hash - (value of leftmost letter* pown)) * 97 + value of next letter from the right) % 1009
+			content_hash = ((content_hash - ((int)content[i] * pown)) * 97 + content[i + substring.length()]) % 1009;
+			if (content_hash < 0) content_hash += 1009;// in case we get to negatives, we add the prime number to be back in the positives, with the same modulo value 
 		}
 
 	}
@@ -52,18 +53,17 @@ unsigned short rabinKarp(short sub_hash,short content_hash,std::string substring
 }
 
 
-unsigned short subStringCount(std::string fileName, std::string subString )
+unsigned int subStringCount(std::string fileName, std::string subString )
 {
 	
-	int iterator = 0;
-	unsigned short count = 0; // number of times the substring we're looking for has appeared in the text
-	short substring_hash = 0;
-	short content_hash = 0;
+
+	unsigned int count = 0; // number of times the substring we're looking for has appeared in the text. can't possibly need to be a negative value
+	int substring_hash = 0;
+	int content_hash   = 0;
 	
 	std::stringstream stream; // a stringstream that will hold the entire text 
 	std::fstream file; // file that the main text will be loaded from
-	std::string line = ""; //since the string we're looking for is a word, and book is formatted so that words are never split between two lines, we can treat each line separately.
-	// IF that was not the case, we could ensure the substring is not missed by accident because of this by saving the last part of each line and considering it together with the beginning of the next one
+	std::string line = ""; 
 
 	file.open(fileName, std::ios::in);
 
@@ -71,22 +71,26 @@ unsigned short subStringCount(std::string fileName, std::string subString )
 	{
 
 		std::getline(file, line); //get the next line of text
-		stream << line; // hold it in a stringstream temporarily, so we can close the file and have entire content in memory already
+		
+		stream << line << std::endl; // hold it in a stringstream temporarily, so we can close the file and have entire content in memory already
 
 	}
 	file.close();
-
-		
+	
+	line = stream.str();
+		the_clock::time_point pointA = the_clock::now();
 		for (int i = 1; i <= subString.length(); i++)
 		{
-			p = (p * 94) % 1009;
-			substring_hash = (substring_hash * 94 + (short)subString[i - 1]) % 1009;
-			content_hash = (content_hash * 94 + (short)stream.str()[i - 1]) % 1009;
+			pown = (pown * 97) % 1009;
+			substring_hash = (substring_hash * 97 + (int)subString[i - 1]) % 1009;
+			content_hash = (content_hash * 97 + (int)line[i - 1]) % 1009;
 			
 		}
-		count += rabinKarp(substring_hash,content_hash,subString,stream.str()); //find all occurences of the substring within the line
+		count += rabinKarp(substring_hash,content_hash,subString,line); //find all occurences of the substring within the line
 		
-	
+		the_clock::time_point pointB = the_clock::now();
+		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(pointB - pointA).count();
+		std::cout << "It took: " <<0.000001* duration << " seconds." << std::endl;
 	return count;
 }
 
@@ -94,13 +98,14 @@ unsigned short subStringCount(std::string fileName, std::string subString )
 
 int main()
 {
-	std::cout << "count of substrings found: " <<subStringCount("test.txt", "Raskolnikov") ;
+	unsigned int result = 0;
+	
+	result = subStringCount("full.txt", "Raskolnikov");
+	
+
+	
+	std::cout << "count of substrings found: " << result << std::endl;
 	
 	return 0;
 
 }
-/*
-Raskolnikov
-
-
-*/
